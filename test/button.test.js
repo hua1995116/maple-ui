e227e1cec5c457806f0fd53bd065e6da74c34a44
xurenjie <xurenjie@u51.com>
2018-08-08 11:02:24 +0800

    :sparkles: feat button test

--- test/button.test.js
+++ test/button.test.js
@@ -17,11 +17,10 @@ describe('Button', () => {
   it('按钮点击方法调用', () => {
     const Constructor = Vue.extend(Button)
     const vm = new Constructor().$mount()
-    const spy = chai.spy(() => console.log('button is clicked')) // 点击调用函数
-    vm.$on('click', spy)
+    const cb = sinon.fake()
+    vm.$on('click', cb)
     vm.$el.click()
-    expect(spy.__spy.called).to.be.true;
-    // expect(spy).to.have.been.called()
+    expect(cb).to.have.been.called
     vm.$destroy()
   })
 
@@ -37,16 +36,45 @@ describe('Button', () => {
     vm.$destroy()
   })
 
-  it('设置loading', () => {
+  it('设置disabled后样式和不可点击', () => {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        disabled: true
+      }
+    }).$mount()
+    const cb = sinon.fake(() => console.log('button is clicked')) // 点击调用函数
+    vm.$on('click', cb)
+    vm.$el.click()
+    expect(cb).to.not.have.been.called
+    vm.$destroy()
+  })
+
+  it('设置loading后样式和不可点击', () => {
     const Constructor = Vue.extend(Button)
     const vm = new Constructor({
       propsData: {
         loading: true
       }
     }).$mount()
+    const cb = sinon.fake(() => console.log('button is clicked')) // 点击调用函数
     const classes = vm.$el.className
-    expect(classes).to.be.ok
+    vm.$on('click', cb)
+    vm.$el.click()
+    expect(cb).to.not.have.been.called
     expect(classes).to.eq('m-button icon--left disabled--false loading--true')
+    vm.$destroy()
+  })
+
+  it('Icon的定位设置', () => {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'spinner',
+        iconPosition: 'right'
+      }
+    }).$mount()
+    expect(vm.$el.className).to.eq('m-button icon--right disabled--false loading--false')
     vm.$destroy()
   })
 })
