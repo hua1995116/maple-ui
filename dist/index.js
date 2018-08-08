366a132e541bff84762389f9ac2e97a91cbfd8ed
xurenjie <xurenjie@u51.com>
2018-08-09 00:49:20 +0800

    :bulb: update package.json

---
+++ dist/index.js
@@ -0,0 +1,299 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"mUeF":[function(require,module,exports) {
+
+},{"./../fonts/fontawesome-webfont.eot":[["fontawesome-webfont.22751c68.eot","ps49"],"ps49"],"./../fonts/fontawesome-webfont.woff2":[["fontawesome-webfont.80b7baa2.woff2","m+NW"],"m+NW"],"./../fonts/fontawesome-webfont.woff":[["fontawesome-webfont.cf10c944.woff","5s6e"],"5s6e"],"./../fonts/fontawesome-webfont.ttf":[["fontawesome-webfont.ab2a4aff.ttf","1yZn"],"1yZn"],"./../fonts/fontawesome-webfont.svg":[["fontawesome-webfont.900c6cd1.svg","AjgP"],"AjgP"]}],"wFXB":[function(require,module,exports) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+require('../stylesheets/font-awesome-4.7.0/scss/font-awesome');
+
+exports.default = {
+  name: 'm-icon',
+  props: {
+    type: String
+  },
+  computed: {
+    className: function className() {
+      return ['m-icon', 'fa', 'fas', 'fa-' + this.type];
+    }
+  }
+}; //
+//
+//
+        var $8da595 = exports.default || module.exports;
+      
+      if (typeof $8da595 === 'function') {
+        $8da595 = $8da595.options;
+      }
+    
+        /* template */
+        Object.assign($8da595, (function () {
+          var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('i',{class:_vm.className})}
+var staticRenderFns = []
+
+          return {
+            render: render,
+            staticRenderFns: staticRenderFns,
+            _compiled: true,
+            _scopeId: null,
+            functional: undefined
+          };
+        })());
+      
+},{"../stylesheets/font-awesome-4.7.0/scss/font-awesome":"mUeF"}],"iM1m":[function(require,module,exports) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _icon = require('./icon.vue');
+
+var _icon2 = _interopRequireDefault(_icon);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; } //
+//
+//
+//
+//
+//
+//
+//
+//
+
+exports.default = {
+  name: 'm-button',
+  components: {
+    'm-icon': _icon2.default
+  },
+  computed: {
+    buttonClass: function buttonClass() {
+      var _ref;
+
+      return _ref = {}, _defineProperty(_ref, 'icon--' + this.iconPosition, true), _defineProperty(_ref, 'disabled--' + !!this.disabled, true), _defineProperty(_ref, 'loading--' + this.loading, true), _ref;
+    }
+  },
+  props: {
+    icon: String,
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator: function validator(val) {
+        var may = ['left', 'right'];
+        return !!may.includes(val);
+      }
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    emitClick: function emitClick() {
+      if (!!this.loading) return;else return this.$emit('click');
+    }
+  }
+};
+        var $9c2318 = exports.default || module.exports;
+      
+      if (typeof $9c2318 === 'function') {
+        $9c2318 = $9c2318.options;
+      }
+    
+        /* template */
+        Object.assign($9c2318, (function () {
+          var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('button',{staticClass:"m-button",class:_vm.buttonClass,attrs:{"disabled":_vm.disabled},on:{"click":_vm.emitClick}},[(_vm.icon && !_vm.loading)?_c('m-icon',{staticClass:"icon icon__position",attrs:{"type":_vm.icon}}):_vm._e(),_vm._v(" "),(_vm.loading)?_c('m-icon',{staticClass:"icon icon__position loading",attrs:{"type":"spinner"}}):_vm._e(),_vm._v(" "),_c('div',{staticClass:"content"},[_vm._t("default")],2)],1)}
+var staticRenderFns = []
+
+          return {
+            render: render,
+            staticRenderFns: staticRenderFns,
+            _compiled: true,
+            _scopeId: null,
+            functional: undefined
+          };
+        })());
+      
+},{"./icon.vue":"wFXB"}],"h0wh":[function(require,module,exports) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+//
+//
+//
+//
+//
+
+exports.default = {
+  name: 'm-button-group',
+  mounted: function mounted() {
+    Array.from(this.$el.children).forEach(function (ele) {
+      if (ele.nodeName !== 'BUTTON') {
+        console.error('button-group\u4E2Dslot\u4E2D\u5FC5\u987B\u4E3Am-button,\u5143\u7D20\u9519\u8BEF\u7C7B\u578B\uFF1A' + ele.nodeName);
+      }
+    });
+  }
+};
+        var $248c39 = exports.default || module.exports;
+      
+      if (typeof $248c39 === 'function') {
+        $248c39 = $248c39.options;
+      }
+    
+        /* template */
+        Object.assign($248c39, (function () {
+          var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"m-button-group"},[_vm._t("default")],2)}
+var staticRenderFns = []
+
+          return {
+            render: render,
+            staticRenderFns: staticRenderFns,
+            _compiled: true,
+            _scopeId: null,
+            functional: undefined
+          };
+        })());
+      
+},{}],"Focm":[function(require,module,exports) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _button = require('./src/button.vue');
+
+var _button2 = _interopRequireDefault(_button);
+
+var _buttonGroup = require('./src/button-group.vue');
+
+var _buttonGroup2 = _interopRequireDefault(_buttonGroup);
+
+var _icon = require('./src/icon.vue');
+
+var _icon2 = _interopRequireDefault(_icon);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = {
+  Button: _button2.default,
+  ButtonGroup: _buttonGroup2.default,
+  Icon: _icon2.default
+};
+},{"./src/button.vue":"iM1m","./src/button-group.vue":"h0wh","./src/icon.vue":"wFXB"}]},{},["Focm"], null)
+//# sourceMappingURL=/index.map
